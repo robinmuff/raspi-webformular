4ce04854bc83e0fe625ebaba667a6d2c68ac8a17
Robin Muff <robin.muff@bluewin.ch>
2021-10-20 22:29:56 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var port = 80;
+
+var express = require('express');
+const fs = require('fs');
+const { exec } = require("child_process");
+var app = express();
+
+const options = {
+    root: __dirname,
+    dotfiles: 'deny'
+};
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.static('static'));
+
+app.get("/", function(req, res) {
+    res.sendFile("/static/sites/index.html", options)
+});
+app.post("/restart", function(req, res) {
+    exec('/sbin/shutdown -r now');
+});
+app.post("/remove", function(req, res) {
+    let rawdata = fs.readFileSync('static/data/information.json');
+    let infos = JSON.parse(rawdata);
+    let newinfos = [];
+
+    for (var i = 0; i < infos.length; i++) {
+        if (infos[i].website != req.body.websitename) {
+            newinfos.push(infos[i]);
+        }
+    }
+
+    fs.writeFileSync('static/data/information.json', JSON.stringify(newinfos));
+
+    res.redirect("/")
+});
+app.post("/add", function(req, res) {
+    let rawdata = fs.readFileSync('static/data/information.json');
+    let infos = JSON.parse(rawdata);
+
+    infos.push({ "name": req.body.name, "kuerzel": req.body.kuerzel, "website": req.body.website })
+
+    fs.writeFileSync('static/data/information.json', JSON.stringify(infos));
+
+    res.redirect("/")
+});
+
+app.listen(port, function() {
+    console.log('Listening on port ' + port);
+});
---
+++ static/js/index.js
@@ -0,0 +1,37 @@
+loadDocumentValues();
+loadTableData();
+
+function loadDocumentValues() {
+    document.title = location.hostname + " - Website";
+    document.getElementById("title").innerHTML = "Raspberry Pi | <b>" + location.hostname + "</b>";
+}
+
+async function loadTableData() {
+    var response = await fetch("/data/information.json");
+    var data = await response.json();
+
+    document.getElementById("tablebody").innerHTML = "";
+
+    data.forEach(function(element) {
+        document.getElementById("tablebody").innerHTML += "<tr><td>" + element.name + "</td><td>" + element.kuerzel + "</td><td>" + element.website + "</td>" + "<td><button class='delete' onclick=\"deleteRequest(this.parentNode.parentNode.getElementsByTagName('td')[2].innerText)\"><i class='material-icons'>&#xE872;</i></button></td>" + "</tr>";
+    });
+}
+
+async function warning() {
+    var doRestart = confirm('Sind Sie sicher, dass Sie das Dashboard neustarten wollen? (2 Minuten)');
+    if (doRestart) {
+        await fetch("/restart", { method: "POST" });
+    }
+}
+
+function deleteRequest(elem) {
+    fetch("/remove", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({ "websitename": elem })
+    })
+    loadTableData();
+}
